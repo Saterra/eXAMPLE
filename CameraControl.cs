b93b61b41a8665558953a4866b291226732d65e4
Saterra <teatortikovich@gmail.com>
2023-06-30 13:43:47 +0700

    initial commit

---
+++ CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour 
+{
+	public float sens = 600f;
+	public Transform player;
+
+	private float xRot = 0f;
+
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;	
+	}
+	
+	void Update () 
+	{
+		float nX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+		float nY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+		xRot -= nY;
+		xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+		player.Rotate(Vector3.up * nX);
+	}
+}
---
+++ PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour 
+{
+	public CharacterController controller;
+	public float speed = 10f;
+	
+	public float gravity = -9.8f;
+	private Vector3 velocity;
+	public float JumpHeight = 3f;
+
+	public Transform GroundCheck;
+	public float GroundDist = 0.4f;
+	public LayerMask grandMask;
+	private bool isGrand;
+
+	void Update ()
+	 {
+		isGrand = Physics.CheckSphere(GroundCheck.position, GroundDist, grandMask);
+
+		if (isGrand && velocity.y < 0)
+		{
+			velocity.y = -2f;
+		}
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		controller.Move(move * speed * Time.deltaTime);
+
+		if (Input.GetButtonDown("Jump") && isGrand)
+		{
+			velocity.y = Mathf.Sqrt(JumpHeight * -2 * gravity);
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
